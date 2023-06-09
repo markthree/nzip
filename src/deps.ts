e46593c30f8097d643e1f1a777608bb4d393146c
markthree <1801982702@qq.com>
2023-06-09 15:47:26 +0800

    chore: update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,16 +1,16 @@
 export { pipeline, Readable } from "node:stream"
 export * as fflate from "https://cdn.skypack.dev/fflate@0.8.0?min&dts"
 export { createReadStream, createWriteStream } from "node:fs"
-export { Tar } from "https://deno.land/std@0.190.0/archive/tar.ts"
-export { copy } from "https://deno.land/std@0.190.0/streams/copy.ts"
-export { green } from "https://deno.land/std@0.190.0/fmt/colors.ts"
-export { exists, walk } from "https://deno.land/std@0.190.0/fs/mod.ts"
-export { format as prettyBytes } from "https://deno.land/std@0.190.0/fmt/bytes.ts"
+export { Tar } from "https://deno.land/std@0.191.0/archive/tar.ts"
+export { copy } from "https://deno.land/std@0.191.0/streams/copy.ts"
+export { green } from "https://deno.land/std@0.191.0/fmt/colors.ts"
+export { exists, walk } from "https://deno.land/std@0.191.0/fs/mod.ts"
+export { format as prettyBytes } from "https://deno.land/std@0.191.0/fmt/bytes.ts"
 export {
   basename,
   join,
   relative,
-} from "https://deno.land/std@0.190.0/path/mod.ts"
+} from "https://deno.land/std@0.191.0/path/mod.ts"
 export {
   Command,
   EnumType,
