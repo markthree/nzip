0672d85c9573ed87ac28218f74c9f6d1c7fc13a4
markthree <1801982702@qq.com>
2023-07-23 16:48:16 +0800

    chore: update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,25 +1,25 @@
 export { pipeline, Readable } from "node:stream"
 export * as fflate from "https://cdn.skypack.dev/fflate@0.8.0?min&dts"
 export { createReadStream, createWriteStream } from "node:fs"
-export { Tar, Untar } from "https://deno.land/std@0.191.0/archive/mod.ts"
-export { copy } from "https://deno.land/std@0.191.0/streams/copy.ts"
-export { green } from "https://deno.land/std@0.191.0/fmt/colors.ts"
+export { Tar, Untar } from "https://deno.land/std@0.195.0/archive/mod.ts"
+export { copy } from "https://deno.land/std@0.195.0/streams/copy.ts"
+export { green } from "https://deno.land/std@0.195.0/fmt/colors.ts"
 export {
   ensureDir,
   ensureFile,
   exists,
   walk,
-} from "https://deno.land/std@0.191.0/fs/mod.ts"
-export { format as prettyBytes } from "https://deno.land/std@0.191.0/fmt/bytes.ts"
+} from "https://deno.land/std@0.195.0/fs/mod.ts"
+export { format as prettyBytes } from "https://deno.land/std@0.195.0/fmt/bytes.ts"
 export {
   basename,
   join,
   relative,
-} from "https://deno.land/std@0.191.0/path/mod.ts"
+} from "https://deno.land/std@0.195.0/path/mod.ts"
 export {
   Command,
   EnumType,
-} from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts"
+} from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/mod.ts"
 
 export { default as AdmZip } from "npm:adm-zip-iconv@0.4.9";
 
