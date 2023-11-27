a80aba268159df20c7f06f1725edb3791e1664c7
markthree <1801982702@qq.com>
2023-11-27 12:49:52 +0800

    chore: update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,21 +1,21 @@
 export { pipeline, Readable } from "node:stream";
 export * as fflate from "https://cdn.skypack.dev/fflate@0.8.0?min&dts";
 export { createReadStream, createWriteStream } from "node:fs";
-export { Tar, Untar } from "https://deno.land/std@0.201.0/archive/mod.ts";
-export { copy } from "https://deno.land/std@0.201.0/streams/copy.ts";
-export { green } from "https://deno.land/std@0.201.0/fmt/colors.ts";
+export { Tar, Untar } from "https://deno.land/std@0.208.0/archive/mod.ts";
+export { copy } from "https://deno.land/std@0.208.0/streams/copy.ts";
+export { green } from "https://deno.land/std@0.208.0/fmt/colors.ts";
 export {
   ensureDir,
   ensureFile,
   exists,
   walk,
-} from "https://deno.land/std@0.201.0/fs/mod.ts";
-export { format as prettyBytes } from "https://deno.land/std@0.201.0/fmt/bytes.ts";
+} from "https://deno.land/std@0.208.0/fs/mod.ts";
+export { format as prettyBytes } from "https://deno.land/std@0.208.0/fmt/bytes.ts";
 export {
   basename,
   join,
   relative,
-} from "https://deno.land/std@0.201.0/path/mod.ts";
+} from "https://deno.land/std@0.208.0/path/mod.ts";
 export {
   Command,
   EnumType,
