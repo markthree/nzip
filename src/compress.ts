2fd84a778c29275ff4fd69aba3371ec9986c738d
markthree <1801982702@qq.com>
2024-01-30 18:15:54 +0800

    refactor(nzip): use zip.js

--- src/compress.ts
+++ src/compress.ts
@@ -1,4 +1,4 @@
-import { AdmZip, Buffer, copy, Tar } from "./deps.ts";
+import { copy, ensureFile, Tar, ZipWriter } from "./deps.ts";
 
 export async function tar(files: string[], output: string) {
   const tar = new Tar();
@@ -9,20 +9,36 @@ export async function tar(files: string[], output: string) {
       filePath: file,
     });
   }
-  const writer = await Deno.open(output, { write: true, create: true });
+  await ensureFile(output);
+  const writer = await Deno.open(output, { write: true });
   await copy(tar.getReader(), writer);
   writer.close();
 }
 
-export async function zip(files: string[], output: string) {
-  const zip = new AdmZip();
+interface ZipOptions {
+  /**
+   * @default false
+   */
+  useWebWorkers?: boolean;
+}
 
+export async function zip(
+  files: string[],
+  output: string,
+  options?: ZipOptions,
+) {
+  await ensureFile(output);
+  using fd = await Deno.open(output, { write: true });
+  const zipWriter = new ZipWriter(fd);
+  const { useWebWorkers = false } = options || {};
   const promises = files.map(async (file) => {
-    const content = await Deno.readFile(file);
-    zip.addFile(file, Buffer.from(content));
+    using fd = await Deno.open(file, { read: true });
+    await zipWriter.add(file, fd, {
+      useWebWorkers,
+    });
   });
 
   await Promise.all(promises);
 
-  await zip.writeZipPromise(output);
+  await zipWriter.close();
 }
